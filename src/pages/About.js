39618235bf3150b754aa72fcaefbf8e3c3a62c42
SethCohen <sethcohen.dev@gmail.com>
2021-04-21 01:27:26 -0400

    Cleaning repo

--- src/pages/About.js
+++
@@ -1,9 +0,0 @@
-import {Typography} from "@material-ui/core";
-
-export default function About(){
-    return (
-        <Typography variant="h1">
-            About Page
-        </Typography>
-    );
-}
--- src/pages/Portfolio.js
+++
@@ -1,9 +0,0 @@
-import {Typography} from "@material-ui/core";
-
-export default function Portfolio(){
-    return (
-        <Typography variant="h1">
-            Portfolio page
-        </Typography>
-    );
-}
