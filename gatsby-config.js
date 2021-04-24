125bd9d64b0f278db3821dbd71f1249f0f1e5cb5
SethCohen <sethcohen.dev@gmail.com>
2021-04-24 18:44:08 -0400

    Updated project
    - Implemented rss feed support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
+    siteUrl: `https://www.sethdev.ca/`,
     title: `SethDev`,
     description: `Seth's Personal Website & Blog`,
-    author: `Seth Cohen <sethcohen.dev@gmail.com>`,
+    author: `Seth Cohen <sethcohen.dev@gmail.com>`
   },
   plugins: [
     `gatsby-theme-material-ui`,
@@ -12,16 +13,16 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
-        path: `${__dirname}/src/`,
-      },
+        path: `${__dirname}/src/`
+      }
     },
     {
       resolve: `gatsby-plugin-redirect-to`,
       options: {
         force: true,
         isPermanent: true,
-        redirectInBrowser: true,
-      },
+        redirectInBrowser: true
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -29,9 +30,18 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `sethdev`,
+        short_name: `sethdev`,
         start_url: `/`,
+        background_color: `#191c1e`,
+        theme_color: `#4069B2`,
+        display: `standalone`,
         icon: `src/assets/logo.png`, // This path is relative to the root of the site.
-      },
+        icon_options: {
+          // For all the options available,
+          // please see the section "Additional Resources" below.
+          purpose: `any maskable`,
+        },
+      }
     },
     {
       resolve: "gatsby-plugin-react-svg",
@@ -43,8 +53,61 @@ module.exports = {
     },
     `gatsby-transformer-remark`,
     `gatsby-plugin-gatsby-cloud`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }]
+                });
+              });
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "SethDev RSS Feed"
+          }
+        ]
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
-}
+  ]
+};
