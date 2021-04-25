f54f0e699a3e0f8028a15679347b9f8cd99162bf
SethCohen <sethcohen.dev@gmail.com>
2021-04-25 05:21:07 -0400

    Updated project
    - Minor tweak to art.js; changing Seo title
    - Implemented /Programming/ category view

--- src/pages/portfolio/art.js
+++ src/pages/portfolio/art.js
@@ -3,10 +3,19 @@ import { graphql } from "gatsby";
 import Header from "../../components/header";
 import Seo from "../../components/seo";
 import Gallery from '@browniebroke/gatsby-image-gallery'
-import { Container } from "@material-ui/core";
+import { Container, makeStyles } from "@material-ui/core";
 
+const useStyles = makeStyles((theme) => ({
+  padding: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8)
+  },
+
+}));
 
 export default function Art({ data }) {
+  const classes = useStyles();
+
   const images = data.allFile.edges.map(({ node }) => ({
     ...node.childImageSharp,
     caption: node.childImageSharp.meta.originalName,
@@ -15,8 +24,8 @@ export default function Art({ data }) {
   return (
     <>
       <Header siteTitle="// Seth Cohen" />
-      <Seo title="Portfolio" />
-      <Container><Gallery images={images} /></Container>
+      <Seo title="Art" />
+      <Container className={classes.padding}><Gallery images={images} /></Container>
     </>
   );
 
