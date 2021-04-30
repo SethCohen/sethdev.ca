4966648d036b49b8e10961b5bf02285c1aacf066
SethCohen <sethcohen.dev@gmail.com>
2021-04-29 21:17:30 -0400

    Updated project
    - Start of svg hover change styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,8 @@ import Fiverr from "../assets/fiverr.svg";
 import Seo from "../components/seo";
 import { Container, Grid, Grow, Tooltip } from "@material-ui/core";
 import Header from "../components/header";
+import "../components/svgStyle.css";
+
 
 export default function Index() {
 
@@ -26,28 +28,37 @@ export default function Index() {
           >
             <Grid item>
               <Tooltip title="Email" aria-label="Email">
-              <a href="mailto:sethcohen.dev@gmail.com" aria-label="Mail"><Email /></a>
+                <a href="mailto:sethcohen.dev@gmail.com" aria-label="Mail">
+                  <Email className="email" />
+                </a>
               </Tooltip>
             </Grid>
             <Grid item>
               <Tooltip title="Github" aria-label="Github">
-              <a href="https://github.com/SethCohen" aria-label="Github"><Github /></a>
+                <a href="https://github.com/SethCohen" aria-label="Github">
+                  <Github className="github" />
+                </a>
               </Tooltip>
             </Grid>
             <Grid item>
               <Tooltip title="Playstore" aria-label="Playstore">
-              <a href="https://play.google.com/store/apps/developer?id=SethCohen"
-                 aria-label="Playstore"><Playstore /></a>
+                <a href="https://play.google.com/store/apps/developer?id=SethCohen" aria-label="Playstore">
+                  <Playstore className="playstore" />
+                </a>
               </Tooltip>
             </Grid>
             <Grid item>
               <Tooltip title="Youtube" aria-label="Youtube">
-              <a href="https://www.youtube.com/channel/UCt3r8b3iDiUiQo9SdBRBgNw" aria-label="Youtube"><Youtube /></a>
+                <a href="https://www.youtube.com/channel/UCt3r8b3iDiUiQo9SdBRBgNw" aria-label="Youtube">
+                  <Youtube className="youtube" />
+                </a>
               </Tooltip>
             </Grid>
             <Grid item>
               <Tooltip title="Fiverr" aria-label="Fiverr">
-              <a href="https://www.fiverr.com/" aria-label="Fiverr"><Fiverr /></a>
+                <a href="https://www.fiverr.com/" aria-label="Fiverr">
+                  <Fiverr className="fiverr" />
+                </a>
               </Tooltip>
             </Grid>
           </Grid>
