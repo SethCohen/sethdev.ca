cf6b4f94b229de1e124752828c82adb7b01ffe40
SethCohen <sethcohen.dev@gmail.com>
2021-05-01 13:06:02 -0400

    Update index.js
    Added some about me info

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Github from "../assets/icons/github.svg";
 import Email from "../assets/icons/email.svg";
 import Fiverr from "../assets/icons/fiverr.svg";
 import Seo from "../components/seo";
-import { Container, Grid, Grow, Tooltip } from "@material-ui/core";
+import { Box, Container, Grid, Grow, Tooltip, Typography } from "@material-ui/core";
 import Header from "../components/header";
 import "../components/svgStyle.css";
 
@@ -41,14 +41,16 @@ export default function Index() {
             </Grid>
             <Grid item>
               <Tooltip title="Playstore" aria-label="Playstore">
-                <a href="https://play.google.com/store/apps/developer?id=SethCohen" target="_blank" rel="noreferrer noopener" aria-label="Playstore">
+                <a href="https://play.google.com/store/apps/developer?id=SethCohen" target="_blank"
+                   rel="noreferrer noopener" aria-label="Playstore">
                   <Playstore className="playstore" />
                 </a>
               </Tooltip>
             </Grid>
             <Grid item>
               <Tooltip title="Youtube" aria-label="Youtube">
-                <a href="https://www.youtube.com/channel/UCt3r8b3iDiUiQo9SdBRBgNw" target="_blank" rel="noreferrer noopener" aria-label="Youtube">
+                <a href="https://www.youtube.com/channel/UCt3r8b3iDiUiQo9SdBRBgNw" target="_blank"
+                   rel="noreferrer noopener" aria-label="Youtube">
                   <Youtube className="youtube" />
                 </a>
               </Tooltip>
@@ -61,6 +63,31 @@ export default function Index() {
               </Tooltip>
             </Grid>
           </Grid>
+          <Box style={{ minHeight: "30vh" }}></Box>
+          <Box style={{ minHeight: "70vh" }}>
+            <Typography align="center" variant="h4">
+              Hi, I'm Seth! 🍵
+              <br /><br />
+              I'm a freelancer and passionate hobbyist for creating various apps & software as well as a bit of digital
+              art on the side. I like to follow a DIY ideology and learn new skills to accomplish whatever ideas I come
+              up with. I like to work on various digital art or app development projects.
+              <br /><br />
+              My Tech Stack:
+              <br />
+            </Typography>
+            <Typography align="center" variant="h5">
+              Python, Java, Javascript, HTML/CSS, Dart, Lua, C++, Markdown,
+              <br />
+              React, Gatsby, Material UI, Flutter,
+              <br />
+              Aseprite, Adobe Photoshop, Adobe Illustrator, Adobe Premiere Pro, Adobe After Effects,
+              <br />
+              PyCharm, IntellIJ, Webstorm, Android Studio,
+              <br />
+              Firebase, SQLite
+            </Typography>
+
+          </Box>
         </Container>
       </Grow>
     </>
